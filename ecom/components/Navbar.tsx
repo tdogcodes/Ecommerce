fe0d3b5f089c080d4e9613e771513eb16c47e12f
tdogcodes <trxcycsgo@gmail.com>
2025-03-21 16:02:15 -0700

    hero section

--- ecom/components/Navbar.tsx
+++ ecom/components/Navbar.tsx
@@ -1,16 +1,17 @@
 import Link from 'next/link'
 
-export const Navbar = () => {
+export default function Navbar(){
   return (
-    <nav>
-        <div>
+    <nav className="sticky top-0 z-50 bg-white shadow">
+        <div className="container mx-auto flex items-center justify-between px-4 py-4">
             <Link href='/'>Leftwing Patriots</Link>
-        </div>
-        <div>
-            <Link href='/'>Home</Link>
-            <Link href='/products'>Products</Link>
-            <Link href='/checkout'>Checkout</Link>
-        </div>
+            <div className="hidden md:flex space-x-6">
+              <Link href='/' className="hover:text-blue-600">Home</Link>
+              <Link href='/products' className="hover:text-blue-600">Products</Link>
+              <Link href='/checkout' className="hover:text-blue-600">Checkout</Link>
+            </div>
+            <div className="flex items-center space-x-4"></div>
+        </div> 
     </nav>
   )
 }
